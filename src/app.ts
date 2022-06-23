939a50537026406b115dcf125b53a71a21e2ed51
tdam <tobias.dam@fhstp.ac.at>
2022-06-23 22:33:42 +0200

    - added CORS headers - added devices endpoint

--- src/app.ts
+++ src/app.ts
@@ -19,13 +19,18 @@ const channel = createChannel();
 const server = createServer(async (req, res) => {
     switch (req.url) {
         case "/sse": {
+            res.setHeader("Access-Control-Allow-Origin",["http://127.0.0.1:3000"]);
             const session = await createSession(req, res);
             channel.register(session);
-            session.push(devices);
+            session.push(devices, "init");
 
             // session.push("Hello world!");
             // channel.broadcast(devices);
 
+            break;
+        }
+        case "/devices":{
+            res.end(JSON.stringify(devices));
             break;
         }
         default: {
@@ -35,9 +40,9 @@ const server = createServer(async (req, res) => {
 });
 
 function processData(state: { [key: string]: { [key: string]: any } }, db: { [key: string]: any }[]): { [key: string]: Device } {
-    let devices: { [key: string]: Device } = {};
-    for (let ieeeAddr in state) {
-        let dev: { [key: string]: any } = {};
+    const devices: { [key: string]: Device } = {};
+    for (const ieeeAddr in state) {
+        const dev: { [key: string]: any } = {};
         dev["ieeeAddr"] = ieeeAddr;
         dev["attributes"] = Object.keys(state[ieeeAddr]);
         for (let i = 0; i < db.length; i++) {
@@ -52,37 +57,34 @@ function processData(state: { [key: string]: { [key: string]: any } }, db: { [ke
     return devices;
 }
 function readDB(): { [key: string]: any }[] {
-    let dbContent = readFileSync(dbPath, "utf-8");
-    let db: { [key: string]: any }[] = [];
+    const dbContent = readFileSync(dbPath, "utf-8");
+    const db: { [key: string]: any }[] = [];
     dbContent.split(/\r?\n/).filter(n => n).forEach(line => {
         db.push(JSON.parse(line));
     });
     return db;
 }
 
-let state = JSON.parse(readFileSync(statePath, "utf-8"));
-let dbContent = readFileSync(dbPath, "utf-8");
-let db = readDB();
+const state = JSON.parse(readFileSync(statePath, "utf-8"));
+// const dbContent = readFileSync(dbPath, "utf-8");
+const db = readDB();
 
 devices = processData(state, db);
 // console.log(devices);
 
 watch(watchPath, (eventType, filename) => {
     if (eventType === "change") {
-        let newState = JSON.parse(readFileSync(statePath, "utf-8"));
-        let newDB = readDB();
-        let newDevices = processData(newState, newDB);
-        for (let device in newDevices) {
+        const newState = JSON.parse(readFileSync(statePath, "utf-8"));
+        const newDB = readDB();
+        const newDevices = processData(newState, newDB);
+        for (const device in newDevices) {
             if (!(device in (devices))) {
-                console.log("if1");
                 channel.broadcast(newDevices[device]);
                 devices[newDevices['device']['ieeeAddr']] = newDevices[device];
             } else {
-                console.log("else");
-                console.log(JSON.stringify(newDevices[device]));
-                console.log(JSON.stringify(devices[device]));
+                // console.log(JSON.stringify(newDevices[device]));
+                // console.log(JSON.stringify(devices[device]));
                 if (JSON.stringify(newDevices[device]) != JSON.stringify(devices[device])) {
-                    console.log("if2");
                     channel.broadcast(newDevices[device]);
                     devices[newDevices[device]['ieeeAddr']] = newDevices[device];
                 }
